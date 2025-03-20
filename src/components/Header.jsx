a6e9e5adbaf0b6962afa45a1b42612d787803d6d
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-20 17:47:08 +0100

    enhance header layout with navigation and logo styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
 export default function Header() {
     return (
-        <header id="Header" className="">
-            <img src="img/dc-logo.png" alt="DC logo" />
+        <header id="Header" className="d-flex justify-content-between align-items-center">
+            <div id="header-logo">
+                <img src="img/dc-logo.png" alt="DC logo" />
+            </div>
+            <div id="header-nav">
+                <ul className="row gap-1 align-items-end justify-content-center">
+                    <li>CHARACTERS</li>           
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </div>
         </header>
     )
 }
