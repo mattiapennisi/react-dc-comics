06b3a17aa5f8180c5ce832b7617349637a42aa8f
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-20 20:36:13 +0100

    enhance footer with navigation links and logo; improve styles for better layout and usability

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,66 @@
 export default function Footer() {
     return (
         <footer id="Footer">
+
             <section id="footer-nav">
                 <div id="footer-nav-links">
-
+                    <div className="footer-nav-links-element">
+                        <h3>
+                            DC COMICS
+                        </h3>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <h3>
+                            SHOP
+                        </h3>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div className="footer-nav-links-element">
+                        <h3>
+                            DC
+                        </h3>
+                        <ul>
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>  
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                    <div className="footer-nav-links-element">
+                        <h3>
+                            SITES
+                        </h3>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
                 </div>
                 <div id="footer-nav-logo">
                     <img src="../../public/img/dc-logo-bg.png" alt="DC logo in background" />
                 </div>
             </section>
+
             <section id="footer-log">
                 <div id="log-button">SIGN-UP NOW!</div>
                 <div id="social-links">
@@ -32,6 +84,7 @@ export default function Footer() {
                     </div>
                 </div>
             </section>
+
         </footer>
     )
 }
