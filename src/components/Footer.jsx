7ab6e45467ff38280defc502fa13904a7c7adc3c
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-20 19:30:25 +0100

    add footer structure and styles, including navigation links and logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 export default function Footer() {
     return (
         <footer id="Footer">
-            <section id="footer-nav"></section>
-            <section id="footer-log"></section>
+            <section id="footer-nav">
+                <div id="footer-nav-links">
+
+                </div>
+                <div id="footer-nav-logo">
+                    <img src="../../public/img/dc-logo-bg.png" alt="DC logo in background" />
+                </div>
+            </section>
+            <section id="footer-log">
+
+            </section>
         </footer>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import './css/resets.css'
 import './css/utilities.css'
-import './css/main.css'
+import './css/app.css'
 import './css/media.css'
 import App from './App.jsx'
 
