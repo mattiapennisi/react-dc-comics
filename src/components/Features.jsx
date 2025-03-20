a4c1d3742d5874ef93f326e2c0492d8d7c697b3d
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-21 00:16:54 +0100

    add bonus

---
+++ src/components/Features.jsx
@@ -0,0 +1,28 @@
+export default function Features() {
+    return (
+        <section id="main-nav">
+                <ul>
+                    <li>
+                        <img src="../../public/img/buy-comics-digital-comics.png" alt="Digital comics" />
+                        <p>DIGITAL COMICS</p>
+                    </li>
+                    <li>
+                        <img src="../../public/img/buy-comics-merchandise.png" alt="Digital comics" />
+                        <p>DC MERCHANDISE</p>
+                    </li>
+                    <li>
+                        <img src="../../public/img/buy-comics-subscriptions.png" alt="Digital comics" />
+                        <p>SUBSCRIPTION</p>
+                    </li>
+                    <li>
+                        <img src="../../public/img/buy-comics-shop-locator.png" alt="Digital comics" />
+                        <p>COMIC SHOP LOCATOR</p>
+                    </li>
+                    <li>
+                        <img src="../../public/img/buy-dc-power-visa.svg" alt="Digital comics" width="40px" />
+                        <p>DC POWER VISA</p>
+                    </li>
+                </ul>
+            </section>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,13 @@
+import Features from "./Features"
+
 export default function Main() {
     return (
         <main id="Main">
             <section id="main-contents">
                 <h1>--&gt; Content goes here &lt;--</h1>
             </section>
-            <section id="main-nav">
-                <ul>
-                    <li>
-                        <img src="../../public/img/buy-comics-digital-comics.png" alt="Digital comics" />
-                        <p>DIGITAL COMICS</p>
-                    </li>
-                    <li>
-                        <img src="../../public/img/buy-comics-merchandise.png" alt="Digital comics" />
-                        <p>DC MERCHANDISE</p>
-                    </li>
-                    <li>
-                        <img src="../../public/img/buy-comics-subscriptions.png" alt="Digital comics" />
-                        <p>SUBSCRIPTION</p>
-                    </li>
-                    <li>
-                        <img src="../../public/img/buy-comics-shop-locator.png" alt="Digital comics" />
-                        <p>COMIC SHOP LOCATOR</p>
-                    </li>
-                    <li>
-                        <img src="../../public/img/buy-dc-power-visa.svg" alt="Digital comics" width="40px" />
-                        <p>DC POWER VISA</p>
-                    </li>
-                </ul>
-            </section>
+            
+            <Features />
         </main>
     )
 }
