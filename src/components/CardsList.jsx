6b007db0b550185b4bcb4f99b0f8cd7228a274f8
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-21 13:01:35 +0100

    add comics data and create Card and CardsList components for displaying comics

---
+++ src/components/CardsList.jsx
@@ -0,0 +1,7 @@
+import Card from './Card'
+
+export default function CardsList() {
+    return (
+        <Card />
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,12 @@
-import Features from "./Features"
+import Features from './Features'
+import CardsList from './CardsList'
 
 export default function Main() {
     return (
         <main id="Main">
+            <section id="jumbotron">
+
+            </section>
             <section id="main-contents">
                 <h1>--&gt; Content goes here &lt;--</h1>
             </section>
